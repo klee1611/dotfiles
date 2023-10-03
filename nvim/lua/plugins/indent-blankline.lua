60981465f82b7e1a6466efd968d38fabf7be4b70
kourtney <kourtneylee1611@gmail.com>
2023-10-03 16:18:30 +0800

    indent-blankline config update

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -4,8 +4,6 @@ return {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
             require("ibl").setup({
-                char = "▏",
-                show_first_indent_level = true,
                 exclude = {
                     filetypes = {
                         'lspinfo',
