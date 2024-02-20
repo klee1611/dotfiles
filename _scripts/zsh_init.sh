1feb057ba224733fd42197037cc4e42779d35eb8
klee1611 <kourtneylee1611@gmail.com>
2024-02-20 14:10:21 +0800

    Add and fix install script, update zsh, nvim settings

--- _scripts/zsh_init.sh
+++ _scripts/zsh_init.sh
@@ -1,23 +1,43 @@
 #!/usr/bin/env bash
+GITHUB_URL="https://github.com"
 
-echo "Installing git if not exists..."
-brew ls --version git || brew install git
+check_plugin_and_install() {
+    local plugin_name=${1}
+    local git_url=${2}
+    local install_path=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/${plugin_name}
 
-echo "Setting up oh-my-zsh as default shell..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+    echo "Checking and installing ${plugin_name}..."
+    [ ! -d "${install_path}" ] && \
+        git clone ${git_url} ${install_path}
+}
 
-### Install plugins ###
-echo "Installing zsh-completions plugin..."
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+main() {
+    echo "Checking and setting up oh-my-zsh as default shell..."
+    if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+    fi
 
-echo "Installing zsh-syntax-highlighting plugin..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "Installing autojump plugin if not exists..."
+    HOMEBREW_NO_AUTO_UPDATE=1 brew ls --version autojump || \
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install autojump
 
-echo "Installing zsh-autosuggestions plugin..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    plugin_list=( "zsh-completions" "zsh-syntax-highlighting" "zsh-autosuggestions" "autoupdate" )
+    git_url_list=(
+        "${GITHUB_URL}/zsh-users/zsh-completions.git"
+        "${GITHUB_URL}/zsh-users/zsh-syntax-highlighting.git"
+        "${GITHUB_URL}/zsh-users/zsh-autosuggestions.git"
+        "${GITHUB_URL}/TamCore/autoupdate-oh-my-zsh-plugins.git"
+    )
+    for i in "${!plugin_list[@]}"; do
+        check_plugin_and_install "${plugin_list[i]}" "${git_url_list[i]}"
+    done
 
-echo "Installing autojump plugin if not exists..."
-brew ls --version autojump || brew install autojump
+    # Maybe need to be installed after zsh-completions set up
+    echo "Checking and installing zsh asdf plugin..."
+    [ ! -d "${HOME}/.asdf" ] && \
+        git clone ${GITHUB_URL}/asdf-vm/asdf.git ~/.asdf
 
-echo "Installing autoupdate plugin..."
-git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autoupdate
+    return 0
+}
+
+main
