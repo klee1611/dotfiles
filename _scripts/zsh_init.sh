b604bb8677ef94a3eaea74f7a12560b52846998d
klee1611 <kourtneylee1611@gmail.com>
2024-02-20 14:10:20 +0800

    zsh init script update

--- _scripts/zsh_init.sh
+++ _scripts/zsh_init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+brew install git
+
 # Set up oh-my-zsh as default shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
 
