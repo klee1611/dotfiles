a63234c6ec63ca8dc400305995a5bcd2cd1506b4
klee1611 <kourtneylee1611@gmail.com>
2024-02-20 14:10:20 +0800

    indent

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -4,29 +4,29 @@ return {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
             require("ibl").setup({
-                    char = "▏",
-                    use_treesitter = true,
-                    show_first_indent_level = true,
-                    filetype_exclude = {
-                        'lspinfo',
-                        'packer',
-                        'checkhealth',
-                        'help',
-                        'man',
-                        'dashboard',
-                        'git',
-                        'markdown',
-                        'text',
-                        'terminal',
-                        'NvimTree',
-                    },
-                    buftype_exclude = {
-                        'terminal',
-                        'nofile',
-                        'quickfix',
-                        'prompt',
-                    },
-                })
+                char = "▏",
+                use_treesitter = true,
+                show_first_indent_level = true,
+                filetype_exclude = {
+                    'lspinfo',
+                    'packer',
+                    'checkhealth',
+                    'help',
+                    'man',
+                    'dashboard',
+                    'git',
+                    'markdown',
+                    'text',
+                    'terminal',
+                    'NvimTree',
+                },
+                buftype_exclude = {
+                    'terminal',
+                    'nofile',
+                    'quickfix',
+                    'prompt',
+                },
+            })
         end
     },
 }
