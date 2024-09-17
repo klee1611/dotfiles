516c578b1f17843c442ccb69f274bf98d20afb48
klee1611 <kourtneylee1611@gmail.com>
2024-09-18 00:26:19 +0800

    Add Copilot chat and use tab instead of spaces for indent

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -26,7 +26,7 @@ opt.laststatus = 3        -- Set global statusline
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true   -- Use spaces instead of tabs
+--opt.expandtab = true   -- Use spaces instead of tabs
 opt.shiftwidth = 4     -- Shift 4 spaces when tab
 opt.tabstop = 4        -- 1 tab == 4 spaces
 opt.smartindent = true -- Autoindent new lines
--- nvim/lua/plugins/basic-plug.lua
+++ nvim/lua/plugins/basic-plug.lua
@@ -1,105 +1,118 @@
 return {
-    -- statusline
-    {
-        'nvim-lualine/lualine.nvim',
-        config = function()
-            require('lualine').setup({
-                options = {
-                    theme = 'onedark',
-                }
-            })
-        end
-    },
-    -- colorscheme
-    {
-        "navarasu/onedark.nvim",
-        lazy = false,
-        priority = 1000,
-        version = "*",
-        config = function()
-            require('onedark').setup {
-                style = 'darker'
-            }
-            require('onedark').load()
-        end
-    },
-    -- LSP related
-    {
-        "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                check_outdated_packages_on_open = false,
-            },
-        },
-    },
-    -- Git diff views
-    { "sindrets/diffview.nvim" },
-    -- autopairs
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {} -- this is equalent to setup({}) function
-    },
-    -- telescope
-    {
-        'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-    },
-    -- file browser extension for telescope
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    },
-    -- Github Copilot
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        config = function()
-            require("copilot_cmp").setup()
-        end
-    },
-    -- Tagbar
-    {
-        "simrat39/symbols-outline.nvim",
-        config = true
-    },
-    -- markdown preview
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
-    -- git
-    {
-        "tpope/vim-fugitive",
-        cmd = "Git",
-        event = 'BufWinEnter',
-    },
-    -- git branch viewer
-    {
-        "rbong/vim-flog",
-        lazy = true,
-        cmd = { "Flog", "Flogsplit", "Floggit" },
-        dependencies = {
-            "tpope/vim-fugitive",
-        },
-    },
-    -- dev container
-    {
-        'jamestthompson3/nvim-remote-containers'
-    }
+	-- statusline
+	{
+		'nvim-lualine/lualine.nvim',
+		config = function()
+			require('lualine').setup({
+				options = {
+					theme = 'onedark',
+				}
+			})
+		end
+	},
+	-- colorscheme
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		priority = 1000,
+		version = "*",
+		config = function()
+			require('onedark').setup {
+				style = 'darker'
+			}
+			require('onedark').load()
+		end
+	},
+	-- LSP related
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				check_outdated_packages_on_open = false,
+			},
+		},
+	},
+	-- Git diff views
+	{ "sindrets/diffview.nvim" },
+	-- autopairs
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		opts = {}     -- this is equalent to setup({}) function
+	},
+	-- telescope
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+	},
+	-- file browser extension for telescope
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+	},
+	-- Github Copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	-- Tagbar
+	{
+		"simrat39/symbols-outline.nvim",
+		config = true
+	},
+	-- markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	-- git
+	{
+		"tpope/vim-fugitive",
+		cmd = "Git",
+		event = 'BufWinEnter',
+	},
+	-- git branch viewer
+	{
+		"rbong/vim-flog",
+		lazy = true,
+		cmd = { "Flog", "Flogsplit", "Floggit" },
+		dependencies = {
+			"tpope/vim-fugitive",
+		},
+	},
+	-- dev container
+	{
+		'jamestthompson3/nvim-remote-containers'
+	}
 }
